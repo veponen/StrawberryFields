c8428230f41808f77077bf15b73fd8df80c8f165
Reijonen Pekka <pekka.reijonen@lapinamk.fi>
2020-04-28 10:31:23 +0300

    making unittest example more clear

--- unittests/calculuspaper1.py
+++ unittests/calculuspaper1.py
@@ -4,8 +4,12 @@ Simple Calculator for mocking
 '''
 class Calculator:
     def sum(self, a, b):
-        time.sleep(10) # long running process
+        #time.sleep(10) # long running process
+        self.sleeping10second()
         return a + b
+
+    def sleeping10second(self):
+        time.sleep(10)
 
 def calculator( a, b):
     return a + b
