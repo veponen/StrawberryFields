07631aafdd937ef64bdc3129cc3b02f7ff35f189
Bui <hien.bui@edu.lapinamk.fi>
2020-04-30 21:46:34 +0300

    add new function to fieldSetting.py

--- dataInflow/fieldSetting.py
+++ dataInflow/fieldSetting.py
@@ -2,5 +2,7 @@ getTempCoefficient = lambda minTemp,maxTemp: abs((minTemp+maxTemp)/2) if minTemp
 getRainCoefficient = lambda rain: rain/10 if (rain != 0) else 1
 getUviCoefficient = lambda uvi: uvi + 1
 getWindCoefficient = lambda wind: wind + 1
+getHumidCofficient = lambda humid: (100-humid)/100
 
 baseVolumeOfIrrigation = 100 #(in mm)
+irrigationRunCount = 3
