a074b80345e802802c0d041538668690e9815351
Bui <hien.bui@edu.lapinamk.fi>
2020-04-30 15:57:53 +0300

    create fieldSetting.py

---
+++ dataInflow/fieldSetting.py
@@ -0,0 +1,6 @@
+getTempCoefficient = lambda minTemp,maxTemp: abs((minTemp+maxTemp)/2) if minTemp+maxTemp !=0 else 1
+getRainCoefficient = lambda rain: rain/10 if (rain != 0) else 1
+getUviCoefficient = lambda uvi: uvi + 1
+getWindCoefficient = lambda wind: wind + 1
+
+baseVolumeOfIrrigation = 100 #(in mm)
