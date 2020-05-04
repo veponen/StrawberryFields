ae3bf7f7f7fcbc5915c02b2bc2641af86d057f67
Bui <hien.bui@edu.lapinamk.fi>
2020-05-05 00:36:06 +0300

    Update Unittest

--- dataInflow/irrigationPrediction_test.py
+++ dataInflow/irrigationPrediction_test.py
@@ -3,16 +3,24 @@ import unittest
 import irrigationPrediction
 class TestIrrigationPrediction(unittest.TestCase):
     final=irrigationPrediction.FinalWeatherData()
+
     def testCoefficent(self):
         coef=irrigationPrediction.df.loc['Cofficient']
         self.assertAlmostEqual(coef.loc['maxTemp'],coef.loc['minTemp'],delta=0.01)
-        self.assertAlmostEqual(coef.loc['wind_3AM'],coef.loc['wind_3PM'],delta=0.001)
+        self.assertAlmostEqual(coef.loc['wind_3AM'],coef.loc['wind_3PM'],delta=0.01)
         self.assertLessEqual(coef.loc['rain'],0)
-    def testClass(self):
+        self.assertGreaterEqual(coef.loc['solarGrad'],0)
+    
+    def testLoad1(self):
+        temp_rain_uv=self.final.load_and_read_from_API_1()
+        self.assertTrue(all([each_day['temp_max'] > each_day['temp_min'] for each_day in temp_rain_uv]))
+        self.assertTrue(all([each_day['uvi_index'] >= 0 for each_day in temp_rain_uv]))
+        self.assertTrue(all([each_day['sunrise']<each_day['sunset'] for each_day in temp_rain_uv]))
+    def dumbTest(self):
         weather_array=self.final.finalizeData()
         dim_array = weather_array.shape
         self.assertEqual(dim_array[1],7)
-        self.assertEqual(dim_array[0],4)
+        self.assertIn(dim_array[0],(4,5))
         moc_1=Mock(return_value=(4,7))
         moc_1.shape=(4,7)
         self.assertEqual(self.final.finalizeData().shape,moc_1())
